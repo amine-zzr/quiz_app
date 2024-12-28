a4c053840dbb86015e4fd64614b183a9916f6ffd
amine-zzr <aminezoukri@gmail.com>
2024-12-28 20:52:24 +0100

    updated user modeel to include a username field

--- models.py
+++ models.py
@@ -5,8 +5,10 @@ from datetime import datetime
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(50), unique=True, nullable=False)
     password_hash = db.Column(db.String(128))
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    quizzes = db.relationship('Quiz', backref='author', lazy=True)
     quiz_results = db.relationship('UserQuizResult', backref='user', lazy=True)
 
 class Quiz(db.Model):
